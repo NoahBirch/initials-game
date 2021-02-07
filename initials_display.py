ba4af40fffcc83aebee1ef9f2f290b8e8351fcca
Noah Birch <Noah@Tobias.hsd1.or.comcast.net>
2021-02-06 19:33:01 -0800

    Factored out the DB

--- initials_display.py
+++ initials_display.py
@@ -1,90 +1,16 @@
 from letter_gen import gen_start
 from collections import OrderedDict
+from initials_database import InitialsDatabase
 import re
 import threading
 import time
 from pynput.keyboard import Key, Controller
-import mysql.connector
 
 keyboard = Controller()
 
 game_over = False
 
-# --------- SQL DB SETUP ---------- #
-
-mydb = mysql.connector.connect(
-	host="localhost",
-	user="monkey2",
-	password="bananas",
-	database="test"
-)
-
-# ------------ DB FUNCTIONS ----------------- #
-def commit_game_answers_to_db(current_game_id, valid_user_id):
-	mycursor = mydb.cursor()
-	# The SQL format line below dictates how we are going to execute one SQL command with the 
-	# data we give it. We have 3 columns we are affecting in the initials_answers table.
-	# initials, answer, and game_id.
-	sql_answer_format = "INSERT INTO initials_answers (initials, answer, game_id, user_id) VALUES (%s, %s, %s, %s)"
-	# Now that we have the format we are passing into an SQL command, 
-	# we need the data. we are getting this from the return_list_for_db function
-	# and we need to pass it the game id we just got. 
-	val_answer = return_list_for_db(current_game_id, valid_user_id)
-
-	# Now that we have our SQL command format and our data setup, we can use the execute many command.
-	# It will execute as many times as the amount of data we pass it in the val_answer list. 
-	mycursor.executemany(sql_answer_format, val_answer)
-	mydb.commit()
-	print(mycursor.rowcount, "rows were inserted!")
-
-def create_game_id():
-	mycursor = mydb.cursor()
-	mycursor.execute("INSERT INTO game VALUE ()")
-	mydb.commit()
-	# Now that we have commited this DB change, we need to grab the last row id which will be the game id.
-	current_game_id = mycursor.lastrowid
-	print ("New game ID created:", current_game_id)
-	return current_game_id
-
-def check_if_first_game(prev_high_score):
-	"""Checks the previous high score value passed to it and returns true if its a valid integer, returns false if not"""
-	try:
-		int(prev_high_score)	
-	except:
-	 	return True
-
-	return False
-
-def compare_scores(current_game_score, prev_high_score, user_id):
-	print(f"This game you got {current_game_score} names.")
-	print(f"{user_id}\'s previous high score is {prev_high_score}")
-	if current_game_score > prev_high_score:
-		return True
-	else:
-		return False
-
-def tally_game_score_primitive(game_id, user_id):
-	# The primitive tally game score method just counts how many names you succesfully commited
-	# to your list. No point values assigned in this function. 
-	mycursor = mydb.cursor()
-	mycursor.execute("SELECT * FROM initials_answers WHERE game_id = '%s' AND answer <> '' AND user_id = '%s' " % (game_id, user_id))
-
-	score = len(mycursor.fetchall())
-	return score
-
-def get_prev_high_score(user_id):
-	mycursor = mydb.cursor()
-	mycursor.execute("SELECT high_score FROM users WHERE user_id = '%s' " % user_id)
-	prev_high_score = mycursor.fetchone()[0]
-	return prev_high_score
-
-def commit_new_high_score(current_game_score, user_id):
-	"""This function will commit the current game score to the DB as the high score"""
-	mycursor = mydb.cursor()
-	sql = "UPDATE users SET high_score = %s WHERE user_id = %s "
-	values = (current_game_score, user_id)
-	mycursor.execute(sql, values)
-	mydb.commit()
+db = InitialsDatabase()
 
 
 # --------- TIMER STUFF ---------- #
@@ -121,64 +47,37 @@ def create_user_answer_dict():
 
 def user_id_loop():
 	# First we need to get user input for our possible user id:
-	user_id_to_check = get_user_id()
+	print("Welcome to the initials game! What is your USER ID?")
+	print("If you want to check your answers against past answers you need to use the same USER ID.")
+	user_id_to_check = input("USER ID: ").strip()
+
 	# Next we check that user input with check_user_id and return an object
-	user_id_match = check_user_id(user_id_to_check)
+	user_id_match = db.check_user_id(user_id_to_check)
 	# Then if that object is empty we know there is no match, so we give the player opportunity to create one.
 	if user_id_match == None:
 		print("There is no USER ID '%s'" % user_id_to_check)
 		print("Would you like to create one?")
 		yn = input("type yes or no and hit RETURN.\n>").lower()
 		if yn == "yes":
-			create_user_id_check(user_id_to_check)
+			user_id_to_add = create_user_id(user_id_to_check)
+			print(f"User {user_id_to_add} added! Welcome to the party!")
 		else:
 			print("Starting over.")
 			user_id_loop()
 	# If there is a match we know we can continue with the game start process.
 	else:
 		print("Great! Welcome back, %s." % user_id_to_check)
-
-	return user_id_to_check
-
-def get_user_id():
-
-	print("Welcome to the initials game! What is your USER ID?")
-	print("If you want to check your answers against past answers you need to use the same USER ID.")
-	user_id_to_check = input("USER ID: ").strip()
-	return user_id_to_check
-
-def check_user_id(user_id_to_check):
-	"""For now this function just returns the given user if its valid, exits program if not"""
-	mycursor = mydb.cursor()
-
-	mycursor.execute("SELECT user_id FROM users WHERE user_id = '%s' " % user_id_to_check)
-
-	return mycursor.fetchone()
 	
-def create_user_id_check(user_id_to_add):
-	print('%r' % user_id_to_add)
-	mycursor = mydb.cursor()
-	mycursor.execute("INSERT INTO users (user_id) VALUES ('%s')" % (user_id_to_add))
-	mydb.commit()
-	print(f"User {user_id_to_add} added! Welcome to the party!")
 	time.sleep(1)
-
+	return user_id_to_check
 
 # ----------- GAMEPLAY FUNCTIONS --------------- #
 
 def display_inits_as_list():
 	# This function displays the users dictionary and any new additions
-	# they have made eveyrtime it is called. 
+	# they have made eveyrtime it is called.
 	for initial_pair in user_dict:
 		print("".join(("".join(initial_pair) + " - ", user_dict[initial_pair])))
-
-def return_list_for_db(game_id, valid_user_id):
-	# This function takes the final user dict of answers and
-	# put it in a list for adding to an SQL DB.
-	db_return_list = []
-	for initial_pair in user_dict:
-		db_return_list.append((("".join(initial_pair)), user_dict[initial_pair], game_id, valid_user_id))
-	return db_return_list
 
 def check_for_game_over():
 	global game_over
@@ -203,9 +102,6 @@ def initial_extraction(i):
 
 	# Now we check our input and grab the beginning letter from each word.
 	extracted_initials = re.findall(r"(\b[a-zA-Z])", i.replace("'", "").upper())
-
-	###Line below used for testing
-	###print("These initials were extracted: ", extracted_initials)
 
 	# Now because players can input more than just two words, we want to check
 	# all the posible combos of initials starting with the first one. 
@@ -268,6 +164,7 @@ def multi_match_processing(i, matches):
 			match_num = take_input()
 			try:
 				if int(match_num) in multiple_match_dict.keys():
+					print("Adding %s to your list." % i)
 					add_to_list(i, matches, (int(match_num)-1))
 					break
 				elif int(match_num) not in multiple_match_dict.keys():	
@@ -322,6 +219,7 @@ def main_gameplay_loop():
 	if len(matches) < 1:
 		no_match_processing()
 	elif len(matches) == 1:
+		print("Adding %s to your list." % i)
 		add_to_list(i, matches, 0)
 	else:
 		multi_match_processing(i, matches)
@@ -345,24 +243,26 @@ def end_game():
 
 
 	# Now to start adding to the DB, we first need to create a game ID.
-	current_game_id = create_game_id()
+	current_game_id = db.create_game_id()
+	print ("New game ID created:", current_game_id)
+
 
 	# Now that we have a game ID, we want to insert our game answers to the DB.
-	commit_game_answers_to_db(current_game_id, valid_user_id)
+	db.commit_game_answers_to_db(current_game_id, valid_user_id, user_dict)
 
 	time.sleep(1)
 
 	###### now we go into our answer processing functions. first we get the current game score and past high score from the DB:
-	current_game_score = tally_game_score_primitive(current_game_id, valid_user_id)
-	prev_high_score = get_prev_high_score(valid_user_id)
+	current_game_score = db.tally_game_score_primitive(current_game_id, valid_user_id)
+	prev_high_score = db.get_prev_high_score(valid_user_id)
 
 	# Now we check if this was their first game. This will return true if it is. 
-	first_game_tf = check_if_first_game(prev_high_score)
+	first_game_tf = db.check_if_first_game(prev_high_score)
 
 	if first_game_tf == True:
 		# if its the first game, we know we definitely want to update their score, cause it will be null on default.
 		print(f"Looks like this is your first game! Setting your brand new high score of {current_game_score}!")
-		commit_new_high_score(current_game_score, valid_user_id)
+		db.commit_new_high_score(current_game_score, valid_user_id)
 		print("Ending game.")
 		exit()
 	else:
@@ -370,12 +270,14 @@ def end_game():
 
 	# Now we know there is a past high score, we need to check if the current game  higher than the past hi score.
 	# This func will return a true answer if the current game is higher. 
-	current_game_higher_tf = compare_scores(current_game_score, prev_high_score, valid_user_id)
+	print(f"This game you got {current_game_score} names.")
+	print(f"{user_id}\'s previous high score  {prev_high_score} names.")
+	current_game_higher_tf = db.compare_scores(current_game_score, prev_high_score, valid_user_id)
 
 	if current_game_higher_tf == True:
 		# if its not a new game we execute this if the current game was higher than the previous high score. 
 		print(f"Awesome, looks like {current_game_score} is your new high score! Updating that now!")
-		commit_new_high_score(current_game_score, valid_user_id)
+		db.commit_new_high_score(current_game_score, valid_user_id)
 
 	else:
 		print("Aww man. You failed to beat your previous high score. Maybe next time!")
