f78e5be82468ada7a29e0df64aa5f072754fef6e
NoahBirch <77862635+NoahBirch@users.noreply.github.com>
2021-02-23 20:03:00 -0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from initials_database import InitialsDatabase
-
-print_players_in_game(13)
-
-db = InitialsDatabase
-
-def print_players_in_game(game_id):
-		print(f"CHECKING GAME {game_id}")
-		print(db.return_all_players_in_game(game_id))
